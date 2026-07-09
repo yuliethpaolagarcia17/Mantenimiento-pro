5f9930d870d9e55b308e44e2a5bb199aa7cd0325
Yulieth Garcia <yuliethpaolagarcia17@gmail.com>
2026-07-09 00:20:24 -0500

    Fix middleware

--- middleware.js
+++ middleware.js
@@ -1,24 +1,5 @@
-import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
-export async function middleware(req) {
-  const res = NextResponse.next()
-  const supabase = createMiddlewareClient({ req, res })
-  const { data: { session } } = await supabase.auth.getSession()
-
-  const isLoginPage = req.nextUrl.pathname === '/login'
-
-  if (!session && !isLoginPage) {
-    return NextResponse.redirect(new URL('/login', req.url))
-  }
-
-  if (session && isLoginPage) {
-    return NextResponse.redirect(new URL('/', req.url))
-  }
-
-  return res
-}
-
-export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
+export function middleware(request) {
+  return NextResponse.next()
 }
